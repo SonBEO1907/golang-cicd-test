8035435d8a54c77cb496ba9083a057ddc7114547
SonBEO1907 <sonnmhe176004@fpt.edu.vn>
2025-03-06 15:01:50 +0700

    add a sample work

--- main.go
+++ main.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/display", GetHelloWorld())
+	r.GET("/display/:number", GetHelloWorld())
 	r.Run(":8080")
 }
 
 func GetHelloWorld() func(*gin.Context) {
 	return func(ctx *gin.Context) {
+		numberParam := ctx.Param("number")
+		if numberParam == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "nil"
+			})
+		}
+		number, err := strconv.Atoi(number)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "format"
+			})
+		}
 		ctx.JSON(http.StatusOK, gin.H{
 			"msg":   "Hello World!!!",
+			"Answer": Square(number),
 		})
 	}
+}
+
+func Square(number int) int {
+	return number * number
 }
