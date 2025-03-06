b6b923e7584e970da05bcff0f283504cd4665624
SonBEO1907 <sonnmhe176004@fpt.edu.vn>
2025-03-06 10:28:42 +0700

    fix main

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 func GetHelloWord() func(*gin.Context) {
 	return func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
-			"msg":   "Hello World"
+			"msg":   "Hello World",
 		})
 	}
 }
